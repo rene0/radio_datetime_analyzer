2968ba50d07af8eed9dbc63935e5a50499fd9441
René Ladan <rene0+github@freedom.nl>
2023-07-19 22:34:06 +0200

    Add tests for dst_info()

--- src/lib.rs
+++ src/lib.rs
@@ -143,6 +143,8 @@ fn str_u8_02(value: Option<u8>) -> String {
 /// # Arguments
 /// * `dst` - current state of daylight saving time
 fn dst_info(dst: Option<u8>) -> String {
+    // DST_ANNOUNCED is mutually exclusive with DST_PROCESSED
+    // see radio_datetime_utils::set_dst()
     let mut s = String::from("");
     if let Some(s_dst) = dst {
         if s_dst & radio_datetime_utils::DST_ANNOUNCED != 0 {
@@ -207,4 +209,124 @@ fn str_jumps(rdt: &RadioDateTimeUtils) -> Vec<&str> {
         jumps.push("Minute jumped");
     }
     jumps
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const DST_EMPTY: &str = "";
+    const DST_SUMMER: &str = "summer";
+    const DST_WINTER: &str = "winter";
+    const DST_ANN_SUMMER: &str = "announced,summer";
+    const DST_ANN_WINTER: &str = "announced,winter";
+    const DST_PROC_SUMMER: &str = "processed,summer";
+    const DST_PROC_WINTER: &str = "processed,winter";
+    const DST_JUMP_SUMMER: &str = "jump,summer";
+    const DST_JUMP_WINTER: &str = "jump,winter";
+    const DST_ANN_JUMP_SUMMER: &str = "announced,jump,summer";
+    const DST_ANN_JUMP_WINTER: &str = "announced,jump,winter";
+    const DST_PROC_JUMP_SUMMER: &str = "processed,jump,summer";
+    const DST_PROC_JUMP_WINTER: &str = "processed,jump,winter";
+
+    #[test]
+    fn test_dst_none() {
+        assert_eq!(dst_info(None), DST_EMPTY);
+    }
+    #[test]
+    fn test_dst_summer() {
+        assert_eq!(dst_info(Some(radio_datetime_utils::DST_SUMMER)), DST_SUMMER);
+    }
+    #[test]
+    fn test_dst_winter() {
+        assert_eq!(dst_info(Some(0)), DST_WINTER);
+    }
+    #[test]
+    fn test_dst_ann_summer() {
+        assert_eq!(
+            dst_info(Some(
+                radio_datetime_utils::DST_ANNOUNCED | radio_datetime_utils::DST_SUMMER
+            )),
+            DST_ANN_SUMMER
+        );
+    }
+    #[test]
+    fn test_dst_ann_winter() {
+        assert_eq!(
+            dst_info(Some(radio_datetime_utils::DST_ANNOUNCED)),
+            DST_ANN_WINTER
+        );
+    }
+    #[test]
+    fn test_dst_proc_summer() {
+        assert_eq!(
+            dst_info(Some(
+                radio_datetime_utils::DST_PROCESSED | radio_datetime_utils::DST_SUMMER
+            )),
+            DST_PROC_SUMMER
+        );
+    }
+    #[test]
+    fn test_dst_proc_winter() {
+        assert_eq!(
+            dst_info(Some(radio_datetime_utils::DST_PROCESSED)),
+            DST_PROC_WINTER
+        );
+    }
+    #[test]
+    fn test_dst_jump_summer() {
+        assert_eq!(
+            dst_info(Some(
+                radio_datetime_utils::DST_JUMP | radio_datetime_utils::DST_SUMMER
+            )),
+            DST_JUMP_SUMMER
+        );
+    }
+    #[test]
+    fn test_dst_jump_winter() {
+        assert_eq!(
+            dst_info(Some(radio_datetime_utils::DST_JUMP)),
+            DST_JUMP_WINTER
+        );
+    }
+    #[test]
+    fn test_dst_ann_jump_summer() {
+        assert_eq!(
+            dst_info(Some(
+                radio_datetime_utils::DST_JUMP
+                    | radio_datetime_utils::DST_ANNOUNCED
+                    | radio_datetime_utils::DST_SUMMER
+            )),
+            DST_ANN_JUMP_SUMMER
+        );
+    }
+    #[test]
+    fn test_dst_ann_jump_winter() {
+        assert_eq!(
+            dst_info(Some(
+                radio_datetime_utils::DST_JUMP | radio_datetime_utils::DST_ANNOUNCED
+            )),
+            DST_ANN_JUMP_WINTER
+        );
+    }
+    #[test]
+    fn test_dst_proc_jump_summer() {
+        assert_eq!(
+            dst_info(Some(
+                radio_datetime_utils::DST_JUMP
+                    | radio_datetime_utils::DST_PROCESSED
+                    | radio_datetime_utils::DST_SUMMER
+            )),
+            DST_PROC_JUMP_SUMMER
+        );
+    }
+    #[test]
+    fn test_dst_proc_jump_winter() {
+        assert_eq!(
+            dst_info(Some(
+                radio_datetime_utils::DST_JUMP | radio_datetime_utils::DST_PROCESSED
+            )),
+            DST_PROC_JUMP_WINTER
+        );
+    }
 }
